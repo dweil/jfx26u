2385445897a9eadb0e41fc7f64217ee963f55912
Daniel Weil <dwl2@3ds.com>
2025-11-06 11:03:00 +0100

    ColorPalette.getCustomColorDialog() PopupWindow.resetOwnerNode + resetOwnerWindow() StyleManager.clearImageCache()
    + fix build test classes
    
    # Conflicts:
    #	modules/javafx.base/src/test/java/test/javafx/event/EventTypeConcurrencyTest.java

--- modules/javafx.graphics/src/main/java/com/sun/javafx/css/StyleManager.java
+++ modules/javafx.graphics/src/main/java/com/sun/javafx/css/StyleManager.java
@@ -832,6 +832,10 @@ final public class StyleManager {
 
     private final ImageCache imageCache = new ImageCache();
 
+    public void clearImageCache() {
+        imageCache.imageCache.clear();
+    }
+
     public Image getCachedImage(String url) {
         return imageCache.getCachedImage(url);
     }
